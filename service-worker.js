a89959b2f53084dfc2ae761bb139616a5750a38b
Dan Mahara <intern.rewasoft@gmail.com>
2025-05-06 12:55:30 +0545

    added progressive web app

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "portfolio-cache-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
